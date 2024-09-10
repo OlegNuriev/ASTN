68764f8897c0d63a089ac3a892c4d081698b5e08
Oleg N <116572824+OlegNuriev@users.noreply.github.com>
2024-09-10 23:49:58 +0300

    Delete less_8 directory

--- less_8/src/main/java/less_8/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package less_8;
-//Класс является пользовательским исключением
-public class MyArrayDataException extends Throwable {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
--- less_8/src/main/java/less_8/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package less_8;
-//Класс является пользовательским исключением
-public class MyArraySizeException extends Throwable {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
