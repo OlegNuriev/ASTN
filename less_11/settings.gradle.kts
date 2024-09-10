db52e2a7300984fd1b10d4d7476ec1d5a9d21805
Oleg N <116572824+OlegNuriev@users.noreply.github.com>
2024-09-10 23:46:03 +0300

    Delete less_11 directory

--- less_11/settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "less_11"
-
--- less_11/src/main/java/less_11/Main.java
+++
@@ -1,9 +0,0 @@
-package less_11;
-
-public class Main {
-    public static void main(String[] args) {
-        ArrayOfWords.start();
-        System.out.println();
-        PhoneDirectory.start();
-    }
-}
